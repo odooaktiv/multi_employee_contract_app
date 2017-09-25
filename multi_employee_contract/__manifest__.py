cab90e055e51c03c76b7768789737d45fe57db5f
Khyati_Nishuta <khyati.nishtatechnolabs@gmail.com>
2017-09-25 17:09:16 +0530

    Initial commit for multi_employee_contract module for Odoo version 10.0

---
+++ multi_employee_contract/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Multiple Employee Contract',
+    'version': '1.0',
+    'author': 'Aktiv Software Pvt. Ltd.',
+    'website': 'http://www.aktivsoftware.com',
+    'summary': 'Create Multiple Contract at a same time.',
+    'category': 'Human Resources',
+    'license': 'AGPL-3',
+    'depends': ['hr_payroll', 'resource'],
+    'data': [
+            'wizard/mutli_contract_view.xml',
+    ],
+    'description': """
+        This module helps to create multiple employee's contracts from a wizard only if the previous contract is in expired state. 
+    """,
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
+
